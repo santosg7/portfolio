05190ad1d1ec030dc2c385fb740e811760ef9ac8
Vanderson Santos <79386865+santosg7@users.noreply.github.com>
2022-01-29 16:57:24 -0300

    commit inicial
    O primeiro commit do site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Front End","Web Developer","is Focused and Determined","Technology"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
